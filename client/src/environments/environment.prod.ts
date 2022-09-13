2dcf7e79923f44a48f5523ef25607ac855066375
conor <conorlyness@gmail.com>
2022-09-13 18:49:06 +0100

    replicate environment for production

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  getAllRunsUrl: 'http://localhost:3001/allRuns',
+  getSpecificRunsUrl: 'http://localhost:3001/specificRuns',
+  logNewRunUrl: 'http://localhost:3001/logRun',
+  editRunUrl: 'http://localhost:3001/editRun',
 };
