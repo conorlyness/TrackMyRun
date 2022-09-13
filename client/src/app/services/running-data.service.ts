a24d90bb9c313ca2635bd455c54925768fe86121
conor <conorlyness@gmail.com>
2022-09-13 18:47:08 +0100

    updates to running service

--- client/src/app/services/running-data.service.ts
+++ client/src/app/services/running-data.service.ts
@@ -4,26 +4,31 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RunningDataService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllRuns() {
     const url = environment.getAllRunsUrl;
-    return this.http.get<Observable<any>>(url, {})
+    return this.http.get<Observable<any>>(url, {});
   }
 
   getSpecificRuns(start: any, end: any) {
-    const body = {start: start, end: end};
-    const url = environment.getSpecificRuns
-    return this.http.post<Observable<any>>(url, body)
+    const body = { start: start, end: end };
+    const url = environment.getSpecificRunsUrl;
+    return this.http.post<Observable<any>>(url, body);
   }
 
   addNewRun(runDate: any, distance: number, notes: string) {
-    const body = {date: runDate, distance: distance, notes: notes};
-    const url = environment.logNewRun;
+    const body = { date: runDate, distance: distance, notes: notes };
+    const url = environment.logNewRunUrl;
+    return this.http.post<Observable<any>>(url, body);
+  }
+
+  editRun(runDate: any, distance: number, notes: string) {
+    const body = { date: runDate, distance: distance, notes: notes };
+    const url = environment.editRunUrl;
     return this.http.post<Observable<any>>(url, body);
   }
 }
