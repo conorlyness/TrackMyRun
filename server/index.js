6806c878b92a6e98432b49ff8452cb8baba5ea47
conor <conorlyness@gmail.com>
2022-09-14 20:32:21 +0100

    image upload and retrieval endpoints

--- server/index.js
+++ server/index.js
@@ -1,13 +1,59 @@
 const express = require('express');
 const db = require('./db');
 const cors = require('cors');
+const multer = require('multer');
+const fs = require('fs');
+var path = require('path');
 const app = express();
 const API_PORT = 3001;
 
 app.use(express.json());
 app.use(cors());
 
-//API routes
+// express.static to make the images im the upload folder accessible
+app.use(express.static('uploads'));
+var allImages = [];
+
+//path to which the images will be saved
+const PATH = './uploads';
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, PATH);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+  }
+});
+let upload = multer({
+  storage: storage
+});
+
+
+// POST File
+app.post('/api/upload', upload.single('image'), function (req, res) {
+  if (!req.file) {
+    console.log("No file is available!");
+    return res.send({
+      success: false
+    });
+  } else {
+    console.log('File is available!');
+    return res.send({
+      success: true
+    })
+  }
+});
+
+app.get('/allImages', async (req, res) => {
+  allImages = [];
+  res.set('Content-Type', 'image/jpeg');
+  fs.readdirSync('./uploads').forEach((file) => {
+    var img = 'http://localhost:3001/' + file;
+    allImages.push(img);
+  });
+
+    res.send(allImages); 
+  });
 
 app.get('/allRuns', async (req, res) => {
     console.log("calling /allRuns");
