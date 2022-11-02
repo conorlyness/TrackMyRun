6d01f8350136203223585dc2cafedad8316f6a02
Conor Lyness <conorlyness@gmail.com>
2022-11-02 18:39:03 +0000

    new filters dialog for runs

---
+++ client/src/app/components/filter-dialog/filter-dialog.component.ts
@@ -0,0 +1,86 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-filter-dialog',
+  templateUrl: './filter-dialog.component.html',
+  styleUrls: ['./filter-dialog.component.scss'],
+})
+export class FilterDialogComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<FilterDialogComponent>,
+    private _formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  filters = this._formBuilder.group({
+    last7: this.data.last7,
+    last14: this.data.last14,
+    last30: this.data.last30,
+  });
+
+  last7: any = this.filters.controls['last7'].value;
+  last14: any = this.filters.controls['last14'].value;
+  last30: any = this.filters.controls['last30'].value;
+
+  noFilters: boolean = true;
+
+  ngOnInit(): void {
+    if (this.last7) {
+      this.filters.controls['last14'].disable();
+      this.filters.controls['last30'].disable();
+      this.noFilters = false;
+      this.data.last7 = true;
+    } else if (this.last14) {
+      this.filters.controls['last7'].disable();
+      this.filters.controls['last30'].disable();
+      this.noFilters = false;
+      this.data.last14 = true;
+    } else if (this.last30) {
+      this.filters.controls['last7'].disable();
+      this.filters.controls['last14'].disable();
+      this.noFilters = false;
+      this.data.last30 = true;
+    }
+  }
+
+  checkboxChanged(checkbox: string, checked: boolean) {
+    console.log(checkbox, checked);
+
+    if (checkbox == 'last7' && checked == true) {
+      this.filters.controls['last14'].disable();
+      this.filters.controls['last30'].disable();
+      this.noFilters = false;
+      this.data.last7 = true;
+    } else if (checkbox == 'last14' && checked == true) {
+      this.filters.controls['last7'].disable();
+      this.filters.controls['last30'].disable();
+      this.noFilters = false;
+      this.data.last14 = true;
+    } else if (checkbox == 'last30' && checked == true) {
+      this.filters.controls['last7'].disable();
+      this.filters.controls['last14'].disable();
+      this.noFilters = false;
+      this.data.last30 = true;
+    } else {
+      this.filters.enable();
+      this.data.last7 = false;
+      this.data.last14 = false;
+      this.data.last30 = false;
+      this.noFilters = true;
+    }
+  }
+
+  onNoClick(): void {
+    this.data.cancel = true;
+    this.dialogRef.close(this.data);
+  }
+
+  resetToDefault() {
+    this.data.last7 = false;
+    this.data.last14 = false;
+    this.data.last30 = false;
+    this.data.resetToDefault = true;
+  }
+}
