8a92cb6aea22936850734c4e14f6010dfe6fc4b4
Conor Lyness <Conorlyness@gmail.com>
2022-11-14 15:36:08 +0000

    pipe observables to catch errors

--- client/src/app/services/running-data.service.ts
+++ client/src/app/services/running-data.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -11,24 +11,40 @@ export class RunningDataService {
 
   getAllRuns() {
     const url = environment.getAllRunsUrl;
-    return this.http.get<Observable<any>>(url, {});
+    return this.http.get<Observable<any>>(url, {}).pipe(
+      retry(1),
+      catchError((error) => this.handleError(error))
+    );
   }
 
   getSpecificRuns(start: any, end: any) {
     const body = { start: start, end: end };
     const url = environment.getSpecificRunsUrl;
-    return this.http.post<Observable<any>>(url, body);
+    return this.http.post<Observable<any>>(url, body).pipe(
+      retry(1),
+      catchError((error) => this.handleError(error))
+    );
   }
 
   addNewRun(runDate: any, distance: number, notes: string) {
     const body = { date: runDate, distance: distance, notes: notes };
     const url = environment.logNewRunUrl;
-    return this.http.post<Observable<any>>(url, body);
+    return this.http.post<Observable<any>>(url, body).pipe(
+      retry(1),
+      catchError((error) => this.handleError(error))
+    );
   }
 
   editRun(runDate: any, distance: number, notes: string) {
     const body = { date: runDate, distance: distance, notes: notes };
     const url = environment.editRunUrl;
-    return this.http.post<Observable<any>>(url, body);
+    return this.http.post<Observable<any>>(url, body).pipe(
+      retry(1),
+      catchError((error) => this.handleError(error))
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    return throwError(() => new Error(`error caught: ${error.error.message}`));
   }
 }
