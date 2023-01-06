b3a84a4cd3e6179c7b19ec1118acd57e25da1b9d
Conor Lyness <Conorlyness@gmail.com>
2023-01-06 19:28:45 +0000

    code cleanup + use router for navigating

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,33 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'trackMyRun';
-  showFiller = false;
-  @ViewChild('drawer', {static: true}) public sidenav: MatSidenav | undefined;
-
-  
-  constructor(private router: Router) { }
-
-
-  loadLogRun() {
-    this.router.navigate(['/']);
-    this.sidenav?.toggle();
-  }
-
-  loadRunLog() {
-    this.router.navigate(['/log']);
-    this.sidenav?.toggle();
-  }
-
-  loadGallery() {
-    this.router.navigate(['/gallery']);
-    this.sidenav?.toggle();
-  }
+  constructor() {}
 }
