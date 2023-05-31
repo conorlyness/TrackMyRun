f79718a700aac2a8a7a4e268d2517b37c85b663e
Conor Lyness <conorlyness@gmail.com>
2023-05-31 21:09:17 +0100

    rework log run to use stepper

--- client/src/app/components/log-run/log-run.component.ts
+++ client/src/app/components/log-run/log-run.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Inject,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import {
   MatDialog,
@@ -8,6 +15,11 @@ import {
 import * as moment from 'moment';
 import { RunningDataService } from 'src/app/services/running-data.service';
 import { ToastrService } from 'ngx-toastr';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatStepper, StepperOrientation } from '@angular/material/stepper';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ShoesService } from 'src/app/services/shoes.service';
 
 export interface DialogData {
   date: any;
@@ -27,14 +39,25 @@ export class LogRunComponent implements OnInit {
   distance?: number;
   notes: string = '';
   dialogAnswer: any;
-  rpe?: number;
+  rpe!: number;
+  shoe?: string;
+  shoeBrand?: string;
+  shoeName?: string;
   @Output() closeDialog = new EventEmitter();
+  stepperOrientation!: Observable<StepperOrientation>;
+  @ViewChild('stepper') stepper!: MatStepper;
 
   constructor(
     private runningService: RunningDataService,
     public dialog: MatDialog,
-    private toast: ToastrService
-  ) {}
+    private toast: ToastrService,
+    private breakpointObserver: BreakpointObserver,
+    private shoeService: ShoesService
+  ) {
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({ matches }) => (matches ? 'horizontal' : 'vertical')));
+  }
 
   ngOnInit(): void {}
 
@@ -43,48 +66,31 @@ export class LogRunComponent implements OnInit {
       this.toast.error('A distance is required');
     } else if (isNaN(this.distance)) {
       this.toast.error('Distance must be a number');
+    } else if (!this.shoe) {
+      this.toast.error('A shoe must be selected');
     } else {
-      const dialogObj = {
-        date: this.date.value,
-        distance: this.distance,
-        notes: this.notes,
-        rpe: this.rpe,
-      };
-      this.openDialog(dialogObj);
+      this.runningService
+        .addNewRun(
+          moment(this.date.value).format('YYYY-MM-DD'),
+          this.distance,
+          this.notes.replace(/'/g, ''),
+          this.rpe,
+          this.shoe
+        )
+        .subscribe(() => {
+          this.toast.success('Run Successfully Logged');
+          if (this.distance && this.shoeBrand && this.shoeName) {
+            this.shoeService
+              .increaseShoeMileage(this.distance, this.shoeBrand, this.shoeName)
+              .subscribe(() => {
+                this.distance = undefined;
+                this.notes = '';
+                this.shoe = '';
+                this.closeLogForm('logged');
+              });
+          }
+        });
     }
-  }
-
-  openDialog(data: any) {
-    console.log(data);
-    const dialogRef = this.dialog.open(RunInfoDialog, {
-      panelClass: 'confirm-dialog',
-      data: {
-        date: data.date,
-        distance: data.distance,
-        notes: data.notes,
-        rpe: data.rpe,
-      },
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      this.dialogAnswer = result;
-      if (this.dialogAnswer === undefined) {
-        console.log('cancelled selection');
-      } else {
-        this.toast.success('Run Successfully Logged');
-        this.runningService
-          .addNewRun(
-            moment(this.dialogAnswer.date).format('YYYY-MM-DD'),
-            this.dialogAnswer.distance,
-            this.dialogAnswer.notes.replace(/'/g, ''),
-            this.dialogAnswer.rpe
-          )
-          .subscribe();
-        this.distance = undefined;
-        this.notes = '';
-        this.closeLogForm('logged');
-      }
-    });
   }
 
   closeLogForm(event: string) {
@@ -94,24 +100,18 @@ export class LogRunComponent implements OnInit {
   storeEffort(rpe: number) {
     this.rpe = rpe;
   }
-}
 
-@Component({
-  selector: 'confirm-run-dialog',
-  templateUrl: 'confirmRunDialog.html',
-  styleUrls: ['./log-run.component.scss'],
-})
-export class RunInfoDialog {
-  constructor(
-    public dialogRef: MatDialogRef<RunInfoDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
-  ) {}
-
-  onNoClick(): void {
-    this.dialogRef.close();
+  storeShoe(shoe: any) {
+    this.shoe = shoe.displayName;
+    this.shoeBrand = shoe.brand;
+    this.shoeName = shoe.name;
   }
 
-  closeDialog(data: DialogData) {
-    this.dialogRef.close(data);
+  next() {
+    this.stepper.next();
+  }
+
+  previous() {
+    this.stepper.previous();
   }
 }
