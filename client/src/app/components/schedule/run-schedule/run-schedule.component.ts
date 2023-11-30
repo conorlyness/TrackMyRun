bdf3ecda67449e2e38fc422d46f1b1c266e660f5
Conor Lyness <conorlyness@gmail.com>
2023-11-30 19:01:22 +0000

    progress on schedule. hooked up ipc + stats

--- client/src/app/components/schedule/run-schedule/run-schedule.component.ts
+++ client/src/app/components/schedule/run-schedule/run-schedule.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { MatDatepicker } from '@angular/material/datepicker';
 import { MatDialog } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
@@ -9,6 +17,17 @@ import * as moment from 'moment';
 import { EditRunDialogComponent } from '../edit-run-dialog/edit-run-dialog.component';
 import { PreviewRunDialogComponent } from '../preview-run-dialog/preview-run-dialog.component';
 import { CompleteRunDialogComponent } from '../complete-run-dialog/complete-run-dialog.component';
+import { RunningDataService } from 'src/app/services/running-data.service';
+import { NgxGaugeType } from 'ngx-gauge/gauge/gauge';
+import { ElectronService } from 'src/app/services/electron.service';
+import { AnalyticsService } from 'src/app/services/analytics.service';
+
+export type PreviewData = {
+  date: Date;
+  run: ScheduledRun;
+  completed?: boolean;
+  incomplete?: boolean;
+};
 
 @Component({
   selector: 'app-run-schedule',
@@ -16,8 +35,11 @@ import { CompleteRunDialogComponent } from '../complete-run-dialog/complete-run-
   styleUrls: ['./run-schedule.component.scss'],
 })
 export class RunScheduleComponent implements OnInit {
-  monthView: boolean = true;
+  dayNames: string[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  monthView!: boolean;
   currentDate: Date = new Date();
+  //we +1 to month as its 0 based
+  currentMonthFixed: number = new Date().getMonth() + 1;
   startOfWeek: Date = this.getStartOfWeek(this.currentDate);
   selectedMonthYear: Date = new Date();
   monthOnUI: number = this.selectedMonthYear.getMonth() + 1;
@@ -25,6 +47,21 @@ export class RunScheduleComponent implements OnInit {
   weeksInMonth: CalendarDay[][] = [];
   currentWeekDays: CalendarDay[] = [];
   scheduledRuns: ScheduledRun[] = [];
+  daysScheduledThisMonth: number = 0;
+  weeksLoggedMileage: number = 0;
+  weeksScheduledMileage: number = 0;
+  mileageCompletePercentage: number = 0;
+  weeksLongRunDistance: number = 0;
+  monthsTotalMiles: number = 0;
+  nextRaceDate?: any;
+  daysTillRace?: number;
+
+  //mileage Gague values
+  gaugeType: NgxGaugeType = 'arch';
+  gaugeValue!: number;
+  gaugeLabel: string = '';
+  gaugeAppendText: string = '%';
+
   startOfCurrentMonth: Date = new Date(
     this.selectedMonthYear.getFullYear(),
     this.selectedMonthYear.getMonth(),
@@ -39,33 +76,76 @@ export class RunScheduleComponent implements OnInit {
   constructor(
     public dialog: MatDialog,
     private toast: ToastrService,
-    private scheduleService: ScheduleService
+    private scheduleService: ScheduleService,
+    private runningDataService: RunningDataService,
+    private electronService: ElectronService,
+    private analyticsService: AnalyticsService
   ) {}
 
   async ngOnInit(): Promise<void> {
-    console.log('current date::', this.currentDate);
+    //ensure we use same state as last time for showing week/month view
+    if (typeof process !== 'undefined' && process.versions.electron) {
+      this.electronService
+        .onOnce('getScheduleSettings')
+        .then((scheduleViewSetting: string) => {
+          console.log('THE SCHEDULE VIEW SETTING::', scheduleViewSetting[1]);
+          let viewPref = scheduleViewSetting[1];
+          if (viewPref === 'true') this.monthView = true;
+          if (viewPref === 'false') this.monthView = false;
+        })
+        .catch((error: any) => {
+          console.error('Error receiving schedule settings:', error);
+        });
+
+      this.electronService.send('getScheduleSettings');
+    } else {
+      let localStorageView = localStorage.getItem('scheduleMonthView');
+      console.log('local storage month view is::', localStorageView);
+      if (localStorageView === 'false') this.monthView = false;
+      if (localStorageView === 'true') this.monthView = true;
+      if (!localStorageView) this.monthView = true;
+    }
+
+    this.getRaces();
     await this.getScheduledRuns(
       this.startOfCurrentMonth,
       this.endOfCurrentMonth
     );
     this.generateSchedule();
+    await this.getWeeksLoggedMileage();
+    await this.getWeeksScheduledMileage();
+    this.calculateWeeklyMileageGoal();
   }
 
   async getScheduledRuns(start: Date, end: Date) {
-    return new Promise((resolve, reject) => {
-      this.scheduleService.getAllScheduledRuns(start, end).subscribe(
-        (schedule) => {
-          console.log('the scheduled runs in the DB::', schedule);
-          this.scheduledRuns = schedule;
+    //scenarios where start of week may be the last day or so in prev month/same for end of month
+    let scheduleStart = start;
+    let scheduleEnd = end;
+    let endOfCurrentWeek = new Date(this.startOfWeek);
+    endOfCurrentWeek.setDate(endOfCurrentWeek.getDate() + 6);
+    if (scheduleStart > this.startOfWeek) scheduleStart = this.startOfWeek;
+    if (scheduleEnd < endOfCurrentWeek) scheduleEnd = endOfCurrentWeek;
 
-          resolve(this.scheduledRuns);
-        },
-        (err) => reject()
-      );
+    console.log('the start::', scheduleStart);
+    console.log('the end::', scheduleEnd);
+    return new Promise((resolve, reject) => {
+      this.scheduleService
+        .getAllScheduledRuns(scheduleStart, scheduleEnd)
+        .subscribe(
+          (schedule) => {
+            console.log('the scheduled runs in the DB::', schedule);
+            this.scheduledRuns = schedule;
+
+            resolve(this.scheduledRuns);
+          },
+          (err) => reject()
+        );
     });
   }
 
   linkSchedule() {
+    this.daysScheduledThisMonth = 0;
+    this.weeksLongRunDistance = 0;
     // Create a Map for faster lookups
     const runsMap = new Map<string, any>(); // Map<date, run>
 
@@ -79,13 +159,15 @@ export class RunScheduleComponent implements OnInit {
       week.forEach((day: CalendarDay, index: number) => {
         const run = runsMap.get(moment(day.date).format('YYYY-MM-DD'));
         if (run) {
-          console.log(
-            'Found a run that matches a date:',
-            'date:',
-            day.date,
-            'with:',
-            run
-          );
+          //update our monthly stats
+          if (
+            day.date >= this.startOfCurrentMonth &&
+            day.date <= this.endOfCurrentMonth
+          ) {
+            this.daysScheduledThisMonth++;
+            console.log('run distance::', run.distance);
+            this.monthsTotalMiles += +run?.distance;
+          }
 
           // Update the original day object within the array
           this.weeksInMonth[weekIndex][index] = {
@@ -100,22 +182,24 @@ export class RunScheduleComponent implements OnInit {
     this.currentWeekDays.forEach((day, dayIndex) => {
       const run = runsMap.get(moment(day.date).format('YYYY-MM-DD'));
       if (run) {
-        console.log(
-          'Found a run that matches a date:',
-          'date:',
-          day.date,
-          'with:',
-          run
-        );
-
         // Update the original day object within the array
         this.currentWeekDays[dayIndex].scheduledRun = run;
-        console.log(
-          'the day object now IN WEEK ARRAY::',
-          this.currentWeekDays[dayIndex]
-        );
+
+        if (this.currentWeekDays[dayIndex]?.scheduledRun?.distance) {
+          let distance = this.currentWeekDays[dayIndex]?.scheduledRun
+            ?.distance as number;
+
+          if (distance > this.weeksLongRunDistance) {
+            this.weeksLongRunDistance = distance;
+          }
+        }
       }
     });
+
+    console.log(
+      'The maximum long run distance for the week:',
+      this.weeksLongRunDistance
+    );
   }
 
   generateSchedule(firstDayOfMonth?: Date, lastDayOfMonth?: Date) {
@@ -242,6 +326,13 @@ export class RunScheduleComponent implements OnInit {
   //switch between week and month view
   changeCalendarView() {
     this.monthView = !this.monthView;
+    if (this.monthView) {
+      localStorage.setItem('scheduleMonthView', 'true');
+      this.electronService.send('setScheduleMonthView', 'true');
+    } else {
+      localStorage.setItem('scheduleMonthView', 'false');
+      this.electronService.send('setScheduleMonthView', 'false');
+    }
   }
 
   isPreviousMonth(day: Date): boolean {
@@ -256,26 +347,45 @@ export class RunScheduleComponent implements OnInit {
     if (
       day?.scheduledRun &&
       !day.scheduledRun.completed &&
-      day.date <= this.currentDate
+      day.date <= this.currentDate &&
+      !day.scheduledRun.incomplete
     ) {
       scheduleRunDialogRef = this.dialog.open(CompleteRunDialogComponent, {
         data: {
           date: day.date ? day.date : null,
           run: day.scheduledRun,
         },
-        width: '350px',
-        height: '200px',
+        width: '450px',
+        height: '350px',
         disableClose: true,
+        autoFocus: false,
       });
     } else if (day?.scheduledRun) {
+      let dialogData: PreviewData = {
+        date: day.date,
+        run: day.scheduledRun,
+        completed: day.scheduledRun.completed,
+        incomplete: day.scheduledRun.incomplete,
+      };
+      let height;
+      let width;
+      if (!day.scheduledRun.incomplete && !day.scheduledRun.completed) {
+        width = '500px';
+        height = '30rem';
+      } else if (day.scheduledRun.completed) {
+        width = '500px';
+        height = '20rem';
+      } else {
+        width = '650px';
+        height = '15rem';
+      }
+
       //we already have a run scheduled for this day so we open the preview and allow to edit, delete etc.
       scheduleRunDialogRef = this.dialog.open(PreviewRunDialogComponent, {
-        data: {
-          date: day.date ? day.date : null,
-          run: day.scheduledRun,
-        },
-        width: '500px',
-        height: '30rem',
+        data: dialogData,
+        width: width,
+        height: height,
+        autoFocus: false,
       });
     } else {
       scheduleRunDialogRef = this.dialog.open(AddRunDialogComponent, {
@@ -285,6 +395,7 @@ export class RunScheduleComponent implements OnInit {
         width: '500px',
         height: '650px',
         disableClose: true,
+        autoFocus: false,
       });
     }
 
@@ -295,6 +406,66 @@ export class RunScheduleComponent implements OnInit {
         this.endOfCurrentMonth
       );
       this.generateSchedule(this.startOfCurrentMonth, this.endOfCurrentMonth);
+      this.getRaces();
+    });
+  }
+
+  async getWeeksLoggedMileage() {
+    this.weeksLoggedMileage = 0;
+
+    return new Promise((resolve, reject) => {
+      this.analyticsService.getCurrentWeekTotal().subscribe(
+        (val) => {
+          this.weeksLoggedMileage = val.total_distance_week
+            ? val.total_distance_week
+            : 0;
+          resolve(this.weeksLoggedMileage);
+        },
+        (err) => reject()
+      );
+    });
+  }
+
+  async getWeeksScheduledMileage() {
+    this.weeksScheduledMileage = 0;
+    return new Promise((resolve, reject) => {
+      if (!this.currentWeekDays) reject();
+      this.currentWeekDays.forEach((day) => {
+        if (day.scheduledRun) {
+          this.weeksScheduledMileage += +day.scheduledRun.distance;
+        }
+      });
+      resolve(this.weeksScheduledMileage);
+    });
+  }
+
+  calculateWeeklyMileageGoal() {
+    // Calculate the percentage complete
+    this.mileageCompletePercentage =
+      (this.weeksLoggedMileage / this.weeksScheduledMileage) * 100;
+    this.gaugeValue = +this.mileageCompletePercentage.toFixed(1);
+    this.gaugeLabel = `${this.weeksLoggedMileage}/${this.weeksScheduledMileage} Miles`;
+  }
+
+  getRaces() {
+    // fetch all data needed for schedule
+    this.scheduleService.getRaceDays().subscribe((races) => {
+      if (races) {
+        for (const race of races) {
+          const raceDate = new Date(race.date);
+          const momentDate = moment(raceDate).format('YYYY-MM-DD');
+          const currentDate = moment().format('YYYY-MM-DD');
+
+          if (momentDate >= currentDate) {
+            this.nextRaceDate = raceDate;
+            const timeTillRace =
+              raceDate.getTime() - this.currentDate.getTime();
+            this.daysTillRace = Math.ceil(timeTillRace / (1000 * 60 * 60 * 24));
+            return; // exit the loop early
+          }
+        }
+      }
+      console.log('ALL RACE DAYS::', races);
     });
   }
 }
