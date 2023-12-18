d59d41ee622d13976497730192936652d66cfbe4
Conor Lyness <conorlyness@gmail.com>
2023-12-18 13:26:20 +0000

    fix bug with no shoe in date search

--- server/sqlQueries/core.js
+++ server/sqlQueries/core.js
@@ -1,6 +1,6 @@
 let coreQueries = {
   viewAllRuns: `SELECT * FROM public.runlog ORDER BY "rundate" DESC;`,
-  viewAllRunsInRange: `select "rundate", "distance", "notes", "rpe", "tags" from "runlog" where "rundate" >= '{start}' and "rundate" <= '{end}';`,
+  viewAllRunsInRange: `select "rundate", "distance", "notes", "rpe", "tags", "shoe" from "runlog" where "rundate" >= '{start}' and "rundate" <= '{end}';`,
   logRun: `Insert into public.runlog ("rundate", "distance", "notes", "rpe", "shoe", "tags") values ('{run.date}', '{run.distance}', '{run.notes}', '{run.rpe}', '{run.shoes}', '{run.tags}');`,
   editRun: `UPDATE public.runlog SET "distance" = '{run.distance}', "notes" = '{run.notes}', "rpe" = '{run.rpe}', "shoe" = '{run.shoe}', "tags" = '{run.tags}' WHERE "rundate" = '{run.date}' AND "id" = '{run.id}';`,
   deleteRun: `DELETE FROM public.runlog WHERE "rundate" = '{run.date}' AND "distance" = '{run.distance}' AND "notes" = '{run.notes}' AND "rpe" = '{run.rpe}' AND "shoe" = '{run.shoe}';`,
